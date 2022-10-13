6b514ebedc82dfcaac24f296fd246b1823d0e6ae
Alexei <datlife2015@yandex.ru>
2022-10-13 09:50:15 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ 004_CubesEvenNum/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine() ?? "0");
+int i = 1;
+
+while (i <= num)
+{
+    int cub = i * i * i;
+    if (cub % 2 == 0)
+    Console.WriteLine($"Куб числа {i} равен {cub}");
+    i++;
+}
+
